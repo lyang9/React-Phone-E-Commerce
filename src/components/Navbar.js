a1e1d11e14e248339e42f1c49178e418d4c06c33
Linda Yang <yanglinda24@gmail.com>
2019-04-21 15:49:14 -0400

    Imported Link from react-router-dom and logo. Rendered Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg';
 
 class Navbar extends Component {
   render() {
     return (
-      <div>
-        <h3>hello from Navbar</h3>
-      </div>
+      <nav className='navbar navbar-expand-sm bg-primary navbar-dark px-sm-5'>
+        <Link to='/'>
+          <img src={logo} alt='store'
+          className='navbar-brand' />
+        </Link>
+        <ul className='navbar-nav align-items-center'>
+          <li className='nav-item ml-5'>
+            <Link to='/' className='nav-link'>
+              products
+            </Link>
+          </li>
+        </ul>
+        <Link to='/cart' className='ml-auto'>
+          <button>
+            <i class="fas fa-cart-plus"></i>
+            my cart
+          </button>
+        </Link>
+      </nav>
     );
   }
 }
