5aff5bc3f33c0885a39a207f1e17fd0354ecc24b
Linda Yang <yanglinda24@gmail.com>
2019-05-19 13:42:49 -0400

    Modified state. componentDidMount for setProducts

--- src/context.js
+++ src/context.js
@@ -7,25 +7,43 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
   state = {
-    products: storeProducts,
+    products: [],
     detailProduct: detailProduct
+  };
+
+  componentDidMount() {
+    this.setProducts();
   }
+
+  setProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach(item => {
+      const singleItem = {...item};
+      tempProducts = [...tempProducts, singleItem];
+    });
+    this.setState(() => {
+      return {products: tempProducts}
+    });
+  };
+  
 
   handleDetail = () => {
     console.log('hello from detail');
-  }
+  };
 
   addToCart = () => {
     console.log('hell0 from add to cart');
-  }
+  };
 
   render() {
     return (
-      <ProductContext.Provider value={{
-        ...this.state,
-        handleDetail: this.handleDetail,
-        addToCart: this.addToCart,
-      }}>
+      <ProductContext.Provider 
+        value={{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart,
+        }}
+      >
         {this.props.children}
       </ProductContext.Provider>
     );
