ce2c6c0ba45079046c40b92751d814385decb2b2
Linda Yang <yanglinda24@gmail.com>
2019-05-19 19:32:47 -0400

    getItem and handleDetail functionality

--- src/context.js
+++ src/context.js
@@ -25,10 +25,17 @@ class ProductProvider extends Component {
       return {products: tempProducts}
     });
   };
-  
 
-  handleDetail = () => {
-    console.log('hello from detail');
+  getItem = (id) => {
+    const product = this.state.products.find(item => item.id === id);
+    return product;
+  }
+
+  handleDetail = (id) => {
+    const product = this.getItem();
+    this.setState(() => {
+      return { detailProduct: product }
+    })
   };
 
   addToCart = (id) => {
